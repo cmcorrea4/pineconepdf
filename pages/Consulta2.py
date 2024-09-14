da6072cd0df22a08e66f00d4ba7add6ac7b20dd4
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-09-14 18:50:14 -0500

    Update Consulta2.py

--- pages/Consulta2.py
+++ pages/Consulta2.py
@@ -6,25 +6,31 @@ from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 import pinecone
 
+
+
+
 # Configurar las credenciales (asegúrate de tener estas variables de entorno configuradas)
 os.environ["OPENAI_API_KEY"] = st.secrets['API_KEY_DE_OPENAI']
 os.environ["PINECONE_API_KEY"] = st.secrets['API_KEY_DE_PINECONE']
 #os.environ["PINECONE_ENV"] = st.secrets["PINECONE_ENV"]
 
-PINECONE_API_KEY = st.secrets['API_KEY_DE_PINECONE']
-OPENAI_API_KEY = st.secrets['API_KEY_DE_OPENAI']
+pc = Pinecone(api_key=PINECONE_API_KEY)
+index_name = 'pdfprueba2'
+dimension = 1536  # Ajusta la dimensión según tu modelo de embeddings
+metric = 'cosine'
+spec = {"serverless": {"cloud": "aws","region": "us-east-1"}}
 
 
 # Inicializar Pinecone
-pinecone.init(
-    api_key=os.environ["PINECONE_API_KEY"],
-    environment=os.environ["PINECONE_ENV"]
-)
+#pinecone.init(
+#    api_key=os.environ["PINECONE_API_KEY"],
+#    environment=os.environ["PINECONE_ENV"]
+#)
 
 # Configurar el embedding y el índice de Pinecone
 embeddings = OpenAIEmbeddings()
 index_name = "pruebapdf2"
-docsearch = Pinecone.from_existing_index(index_name, embeddings)
+docsearch = pc.from_existing_index(index_name, embeddings)
 
 # Configurar el modelo de lenguaje y la cadena de QA
 llm = OpenAI(temperature=0)
@@ -34,7 +40,7 @@ chain = load_qa_chain(llm, chain_type="stuff")
 st.title("Consulta de documentos con Langchain y Pinecone")
 
 # Input para la consulta
-query = st.text_input("Ingresa tu pregunta:")
+query = st.text_area("Ingresa tu pregunta:")
 
 if query:
     # Realizar la búsqueda
