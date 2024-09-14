787705421eb25d739a06dc109502a534bbd4d7bb
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-09-14 14:28:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def extract_text_from_pdf(pdf_file):
     
     reader = PyPDF2.PdfReader(pdf_file)
     text = ''
-    for page_num in range(reader.numPages):
+    for page_num in range( len(reader.pages)):
         page = reader.getPage(page_num)
         text += page.extract_text()
     return text
